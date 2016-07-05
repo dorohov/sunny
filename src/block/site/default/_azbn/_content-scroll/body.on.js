c961c71844de60ca583f6a480479a16e6984373a
caezar47 <iss47@ya.ru>
2016-07-05 11:49:14 +0300

    add new pages

--- src/block/site/default/_azbn/_content-scroll/body.on.js
+++ src/block/site/default/_azbn/_content-scroll/body.on.js
@@ -6,9 +6,22 @@
 		var cs = pcc.find('.content-scroll');
 		
 		//var _h = (parseInt(cs.attr('data-resize-height')) || 50) ;
+		var _h_page = pcc.find('.content-cont.right').attr('data-right-bg');
+
 		
-		var _h = obj.height - 170 - pcc.find('.cont-header').outerHeight(true);
 		
+		if (_h_page == 'none'){
+			var _h = obj.height - 70 - pcc.find('.cont-header').outerHeight(true);
+		}
+		else if (_h_page == 'default-doc'){
+			var _h = obj.height - 280 - pcc.find('.cont-header').outerHeight(true);
+		}
+		else if (_h_page == 'default-gallery'){
+			var _h = obj.height - 140 - pcc.find('.cont-header').outerHeight(true);
+		}
+		else {
+			var _h = obj.height - 170 - pcc.find('.cont-header').outerHeight(true);
+		}
 		cs.css({
 			'height' : _h + 'px',
 			//'margin-top' : obj.height / 3,
